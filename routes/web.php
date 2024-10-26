8d7a1912afa7fbf3d55932f450ed9489bc30619f
zaeem2396 <zaeemansari87@gmail.com>
2024-10-26 21:06:31 +0530

    migration via route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/migrate', function () {
+    Artisan::call('migrate --force');
+    return Artisan::output();
 });
