95e4c308a39151aa0255e389be98e325e09f17f3
zaeem2396 <zaeemansari87@gmail.com>
2024-10-26 21:24:53 +0530

    migration files created

--- database/migrations/2024_10_26_152227_users.php
+++ database/migrations/2024_10_26_152227_users.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('isEmailVerified')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('specialist');
+            $table->string('role');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/migrate', function () {
     Artisan::call('migrate --force');
     return Artisan::output();
