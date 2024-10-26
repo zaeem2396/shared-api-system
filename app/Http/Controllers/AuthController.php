874e8e3e58b1b5b477fe5a3b24087552e4711940
zaeem2396 <zaeemansari87@gmail.com>
2024-10-26 23:40:06 +0530

    author authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+
+    protected $jwtAuth;
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $inputData = $request->only('name', 'email', 'password');
+
+            $validator = Validator::make($inputData, [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email',
+                'password' => 'required|string|min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            $isUserCreated = $this->user->createUser($inputData);
+            return $isUserCreated;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $inputData = $request->only('email', 'password');
+
+            $token = JWTAuth::attempt($inputData);
+
+            if (!$token) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+            $response = [
+                'data' => $this->user->where('email', $inputData['email'])->first(),
+                'token' => $token,
+            ];
+            return response()->json(['message' => 'success', 'response' => $response], 201);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -17,6 +18,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'users';
+
     protected $fillable = [
         'name',
         'email',
@@ -42,4 +45,34 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // Returns a unique identifier for the user (e.g., user ID)
+    public function getJWTIdentifier()
+    {
+        return $this->getKey(); // Usually this is the user's ID
+    }
+
+    // Returns an array of custom claims (you can leave it empty if not using any custom claims)
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public static function createUser(array $inputData)
+    {
+        try {
+            $isUserExist = self::where('email', $inputData['email'])->first();
+            if ($isUserExist) {
+                return response()->json(['message' => 'User already exist'], 400);
+            }
+            $isUserCreated = self::create($inputData);
+            if ($isUserCreated) {
+                return response()->json(['message' => 'User created successfully'], 200);
+            } else {
+                return response()->json(['message' => 'Something went wrong'], 400);
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 }
