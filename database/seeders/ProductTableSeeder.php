1378f99fada94770c49bc6c70ee6ec76acb136f6
zaeem2396 <zaeemansari87@gmail.com>
2025-02-14 23:00:37 +0530

    modified product table seeder

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -21,26 +21,40 @@ class ProductTableSeeder extends Seeder
         try {
             $faker = Faker::create();
             $categoryIds = Category::pluck('id')->toArray();
+            $subCategoryIds = Category::pluck('id')->toArray();
             $vendorIds = User::where('platform_id', app('Helper')->fetchAppSettings()['vendoraPlatformId'])->pluck('id')->toArray();
 
             for ($i = 0; $i < 500; $i++) {
+                $skuId = app(SkuId::class)->generateSkuId();
                 $category = $faker->randomElement($categoryIds);
+                $subCategory = $faker->randomElement($subCategoryIds);
                 $vendor = $faker->randomElement($vendorIds);
                 $productName = $faker->sentence(rand(2, 5));
                 $productDescription = $faker->sentence(rand(20, 35));
                 $productPrice = $faker->randomFloat(2, 10, 1000);
                 $productQuantity = rand(1, 100);
-                // $productImage = $faker->imageUrl(640, 480, 'product', true);
+                $productImage = $faker->imageUrl(640, 480, 'product', true);
 
                 DB::table('products')->insert([
-                    'skuId' => app(SkuId::class)->generateSkuId(),
+                    'skuId' => $skuId,
                     'vendorId' => $vendor,
                     'categoryId' => $category,
+                    'subCategoryId' => $subCategory,
                     'name' => $productName,
                     'description' => $productDescription,
                     'price' => $productPrice,
                     'stock' => $productQuantity,
                     'status' => null,
+                    'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+                    'updated_at' => now(),
+                ]);
+
+                DB::table('product_img')->insert([
+                    'product_id' => $skuId,
+                    'img' => json_encode([
+                        'url' => $productImage,
+                        'public_id' => $faker->uuid,
+                    ]),
                     'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
                     'updated_at' => now(),
                 ]);
